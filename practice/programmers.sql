8a12268547b8ee8de1c535b5c3518634b2154734
Park Hyunggoo <hit135@naver.com>
2022-11-20 20:16:28 +0900

    최댓최솟값, LIKE문, 개수구하기, 여러가지 조건

--- practice/programmers.sql
+++ practice/programmers.sql
@@ -65,3 +65,18 @@ SELECT hire_date
   FROM employees
  WHERE TO_CHAR(hire_date, 'yyyy') = '2002';
 
+
+-----------------------------------------------------------------------------
+
+-- 두가지 이상의 조건,
+-- 두가지 이상의 정렬
+
+-- 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성해주세요. 
+-- 이때 고양이를 개보다 먼저 조회해주세요.
+SELECT ANIMAL_TYPE
+     , COUNT(ANIMAL_TYPE) count
+  FROM ANIMAL_INS
+ WHERE ANIMAL_TYPE = 'Cat'
+    OR ANIMAL_TYPE = 'Dog'
+ GROUP BY ANIMAL_TYPE
+ ORDER BY ANIMAL_TYPE;
